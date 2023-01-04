90cf601626160f641220310837c802b7fabad6f6
Tetraline <danielsamways@gmail.com>
2023-01-04 13:32:45 +0000

    Refactor & add comments to MazeEditor class

--- src/main/java/org/example/MazeVisualizer.java
+++ src/main/java/org/example/MazeVisualizer.java
@@ -11,7 +11,6 @@ public class MazeVisualizer {
     private static final int MAZE_WIDTH = 10;
     private static final int MAZE_HEIGHT = 10;
     private static final Color WALL_COLOR = Color.BLACK;
-    private static final Color BACKGROUND_COLOR = Color.WHITE;
 
     private static class MazePanel extends JPanel {
         private final Maze maze;
@@ -24,26 +23,24 @@ public class MazeVisualizer {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-
             for (int y = 0; y < MAZE_HEIGHT; y++) {
                 for (int x = 0; x < MAZE_WIDTH; x++) {
-                    // TODO: Refactor this into a switch
-                    if (maze.getImage()[y][x] == 1) {
-                        g.setColor(WALL_COLOR);
-                    } else {
-                        g.setColor(BACKGROUND_COLOR);
-                    }
                     if (maze.getImage()[y][x] > 2) {
+                        // AIR
                         if(maze.getImage()[y][x]>2000){
+                            // AIR ON THE ROUTE
                             g.setColor(Color.YELLOW);
                             g.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                             g.setColor(Color.GRAY);
                             g.drawString(Integer.toString(maze.getImage()[y][x]-2000), x * CELL_SIZE + CELL_SIZE / 2 - 10, y * CELL_SIZE + CELL_SIZE / 2);
                         }else {
+                            // AIR NOT ON THE ROUTE
                             g.setColor(Color.GRAY);
                             g.drawString(Integer.toString(maze.getImage()[y][x]), x * CELL_SIZE + CELL_SIZE / 2 - 10, y * CELL_SIZE + CELL_SIZE / 2);
                         }
                     }else {
+                        // WALL
+                        g.setColor(WALL_COLOR);
                         g.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                     }
                 }
@@ -53,13 +50,6 @@ public class MazeVisualizer {
 
     public static void showMaze(Maze maze){
         JFrame frame = new JFrame("Maze Visualizer");
-        //frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //frame.addWindowListener(new WindowAdapter() {
-        //    @Override
-        //    public void windowClosed(WindowEvent e) {
-        //        System.exit(0);
-        //    }
-        //});
 
         // Add the maze panel to the window
         MazePanel panel = new MazePanel(maze);
