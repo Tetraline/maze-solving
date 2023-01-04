848275bff4ac95fc7db9fdcf00db20e0d5efc4b7
Tetraline <danielsamways@gmail.com>
2023-01-04 12:37:59 +0000

    Refactor & add comments to MazeEditor class

--- src/main/java/org/example/MazeEditor.java
+++ src/main/java/org/example/MazeEditor.java
@@ -4,77 +4,77 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Arrays;
-
 import javax.swing.*;
 
+/**
+ * A graphical user interface for creating and editing mazes.
+ * The user can toggle the values of the cells in the maze by clicking on them.
+ * The user can also solve the maze by clicking the "Solve" button.
+ */
 public class MazeEditor extends JPanel {
-  private static final long serialVersionUID = 1L;
+    private static final int CELL_SIZE = 20;
+    private static final int PADDING = 10;
+    private final int[][] maze;
 
-  private static final int CELL_SIZE = 20;
-  private static final int PADDING = 10;
+    public MazeEditor(int[][] maze) {
+        this.maze = maze;
+        setPreferredSize(new Dimension(maze[0].length * CELL_SIZE + PADDING * 2, maze.length * CELL_SIZE + PADDING * 2));
 
-  private final int[][] maze;
-
-  public MazeEditor(int[][] maze) {
-    this.maze = maze;
-    setPreferredSize(new Dimension(maze[0].length * CELL_SIZE + PADDING * 2, maze.length * CELL_SIZE + PADDING * 2));
-
-    addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        int x = (e.getX() - PADDING) / CELL_SIZE;
-        int y = (e.getY() - PADDING) / CELL_SIZE;
-        if (x >= 0 && x < maze[0].length && y >= 0 && y < maze.length) {
-          maze[y][x] = (maze[y][x] + 1) % 2;
-          repaint();
-        }
-      }
-    });
-  }
-
-  @Override
-  public void paintComponent(Graphics g) {
-    g.setColor(Color.WHITE);
-    g.fillRect(0, 0, getWidth(), getHeight());
-
-    g.setColor(Color.BLACK);
-    for (int y = 0; y < maze.length; y++) {
-      for (int x = 0; x < maze[y].length; x++) {
-        if (maze[y][x] == 1) {
-          g.fillRect(PADDING + x * CELL_SIZE, PADDING + y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        }
-      }
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = (e.getX() - PADDING) / CELL_SIZE;
+                int y = (e.getY() - PADDING) / CELL_SIZE;
+                if (x >= 0 && x < maze[0].length && y >= 0 && y < maze.length) {
+                    maze[y][x] = (maze[y][x] + 1) % 2;
+                    repaint();
+                }
+            }
+        });
     }
-  }
 
-  private static void createAndShowGui(int[][] maze) {
-    JFrame frame = new JFrame("Maze Editor");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    @Override
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
-    frame.getContentPane().add(new MazeEditor(maze));
-    JLabel instructionsLabel = new JLabel("Click on cells to toggle between 0 and 1");
-    frame.getContentPane().add(instructionsLabel, BorderLayout.NORTH);
-    JButton saveButton = new JButton("Solve");
-    saveButton.addActionListener(e -> {
-      int[][] mazeArray = Arrays.stream(maze).map(int[]::clone).toArray($ -> maze.clone());
-      Maze m = new Maze(mazeArray);
-      Maze flooded = Flood.flood(m);
-      try {
-        Maze route = Navigator.calculateRoute(flooded);
-        MazeVisualizer.showMaze(route);
-      } catch (Exception ex) {
-        throw new RuntimeException(ex);
-      }
+        g.setColor(Color.BLACK);
+        for (int y = 0; y < maze.length; y++) {
+            for (int x = 0; x < maze[y].length; x++) {
+                if (maze[y][x] == 1) {
+                    g.fillRect(PADDING + x * CELL_SIZE, PADDING + y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+        }
+    }
 
-    });
-    frame.getContentPane().add(saveButton, BorderLayout.SOUTH);
+    private static void createAndShowGui(int[][] maze) {
+        JFrame frame = new JFrame("Maze Editor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    frame.pack();
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-  }
+        frame.getContentPane().add(new MazeEditor(maze));
+        JLabel instructionsLabel = new JLabel("Click on cells to toggle between 0 and 1");
+        frame.getContentPane().add(instructionsLabel, BorderLayout.NORTH);
+        JButton saveButton = new JButton("Solve");
+        saveButton.addActionListener(e -> {
+            int[][] mazeArray = Arrays.stream(maze).map(int[]::clone).toArray($ -> maze.clone());
+            Maze m = new Maze(mazeArray);
+            Maze flooded = Flood.flood(m);
+            try {
+                Maze route = Navigator.calculateRoute(flooded);
+                MazeVisualizer.showMaze(route);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
 
-  public static void run(int[][] maze) {
-    SwingUtilities.invokeLater(() -> createAndShowGui(maze));
-  }
+        });
+        frame.getContentPane().add(saveButton, BorderLayout.SOUTH);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void run(int[][] maze) {
+        SwingUtilities.invokeLater(() -> createAndShowGui(maze));
+    }
 }
